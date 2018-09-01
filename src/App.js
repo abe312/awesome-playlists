00fdc9e60c38681ca2ecd81de48d4d7fda390eec
abe312 <abhineet312@gmail.com>
2018-09-02 00:36:07 +0530

    filter also applies to the songs in each playlist

--- src/App.js
+++ src/App.js
@@ -180,12 +180,14 @@ class App extends Component {
     let headerStyle = {color: green, 'font-size': '50px'}
 
     let playlistsToRender = this.state.user && 
-    this.state.playlists
-    ? this.state.playlists
-    .filter(playlist => 
-      playlist.name.toLowerCase().includes(
-        this.state.filterString.toLowerCase())  
-    ) : [];
+      this.state.playlists
+      ? this.state.playlists.filter(playlist => {
+        let matchesPlaylist = playlist.name.toLowerCase().includes(
+          this.state.filterString.toLowerCase())
+        let matchesSong = playlist.songs.find(song => song.name.toLowerCase()
+          .includes(this.state.filterString.toLowerCase()))
+        return matchesPlaylist || matchesSong
+      }) : []
 
     // let playlistElements = [];
     // if(this.state.serverData.user){
